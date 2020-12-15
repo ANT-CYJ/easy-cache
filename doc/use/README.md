645a8bfefc348aa8410604ba7b14fc37ec6494c3
ANT-CYJ <605601927@qq.com>
2020-12-16 01:06:44 +0800

    modify

---
+++ doc/use/README.md
@@ -0,0 +1,20 @@
+# 使用文档
+
+## 初始化
+
+```js
+var EC = window.EasyCache;
+var cache = new EC();
+```
+
+## 设置
+
+```js
+cache.set("a", 100);
+```
+
+## 获取
+
+```js
+alert(cache.get("a"));
+```
--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
-alert("easy-cache");
+class EasyCache {
+  constructor() {
+    this.list = {};
+  }
+  set(key, value) {
+    this.list[key] = value;
+  }
+  get(key) {
+    return this.list[key];
+  }
+}
+
+window.EasyCache = EasyCache;
